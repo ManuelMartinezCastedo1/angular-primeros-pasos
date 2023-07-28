d301b6035983949e66e614816bb8d50d98b270cb
Manuel Martínez <manuelmartinezcastedo@gmail.com>
2023-07-28 12:39:25 +0200

    Fin de sección 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>Counter: {{counter}}</h3>
+
+  <button (click)="increaseBy()">+1</button>
+  <button (click)="reset()">Reset</button>
+  <button (click)="decreaseBy()">-1</button>
+
+  `
+})
+
+export class CounterComponent  {
+  public counter: number = 10;
+
+  increaseBy():void{
+   this.counter++;
+ }
+  decreaseBy():void{
+   this.counter--;
+ }
+  reset():void{
+   this.counter = 10;
+ }
+}
